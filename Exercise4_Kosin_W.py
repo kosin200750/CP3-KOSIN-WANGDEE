a8a7f5bef637f4497119781669bdb3322e4952f8
kosin200750 <65103054+kosin200750@users.noreply.github.com>
2020-05-10 15:57:51 +0700

    exercise4

---
+++ Exercise4_Kosin_W.py
@@ -0,0 +1,10 @@
+foundeng = 89
+genbusiness = 90
+introcom = 90
+compro = 84
+print("--- Your Score ---")
+print("Foundation English : คะแนนที่ได้",foundeng)
+print("General Business : คะแนนที่ได้",genbusiness )
+print("Introduction to Computer Systems : คะแนนที่ได้",introcom)
+print("Computer Programming : คะแนนที่ได้",compro)
+
