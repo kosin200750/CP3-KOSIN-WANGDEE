6c37fe7073f1d670e34ab1a4dfe42783ab5fadb1
kosin200750 <65103054+kosin200750@users.noreply.github.com>
2020-05-13 14:44:14 +0700

    lecture50

---
+++ Lecture50_Kosin_W.py
@@ -0,0 +1,13 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+
+addNumber(10,25)
+minusNumber(100,50)
+multiplyNumber(60,60)
+divideNumber(150,5)
